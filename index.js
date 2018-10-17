94734dfe63711195019d4b504aa5637fe2b2df4d
Sean Dutton <iceman198@gmail.com>
2018-10-17 10:55:18 -0400

    more

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 8080
 
 app.use(express.static('public'));
-app.get('/index.htm', function (req, res) {
+app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
 
